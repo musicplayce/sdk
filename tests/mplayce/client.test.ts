c1aea62ce3b995d0d895faff0918f80cf7020947
MoonTory | Gustavo Quinta <gustavoqnt40@gmail.com>
2020-07-22 13:24:21 -0300

    fix: assert type string in test

--- tests/mplayce/client.test.ts
+++ tests/mplayce/client.test.ts
@@ -6,12 +6,8 @@ import MusicPlayceClient from '../../lib';
 
 describe('MusicPlayceClient', function () {
 	it('should authenticate with username & password and set the client token', async function () {
-		console.log(
-			'process.env.CLIENT_TEST_USERNAME',
-			process.env.CLIENT_TEST_USERNAME
-		);
-		const username = process.env.CLIENT_TEST_USERNAME;
-		const password = process.env.CLIENT_TEST_PASSWORD;
+		const username = process.env.CLIENT_TEST_USERNAME as string;
+		const password = process.env.CLIENT_TEST_PASSWORD as string;
 
 		const client = await new MusicPlayceClient({ beta: true }).authenticate({
 			username,
