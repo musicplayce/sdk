da06be8fbcdb223661386d9ca545c63f7856bfa9
MoonTory | Gustavo Quinta <gustavoqnt40@gmail.com>
2020-07-22 13:22:12 -0300

    ci: debug env

--- tests/mplayce/client.test.ts
+++ tests/mplayce/client.test.ts
@@ -6,6 +6,10 @@ import MusicPlayceClient from '../../lib';
 
 describe('MusicPlayceClient', function () {
 	it('should authenticate with username & password and set the client token', async function () {
+		console.log(
+			'process.env.CLIENT_TEST_USERNAME',
+			process.env.CLIENT_TEST_USERNAME
+		);
 		const username = process.env.CLIENT_TEST_USERNAME;
 		const password = process.env.CLIENT_TEST_PASSWORD;
 
