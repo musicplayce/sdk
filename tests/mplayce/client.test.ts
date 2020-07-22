920ab75c7d510d9a9e5c7347ef474163e3da35b0
MoonTory | Gustavo Quinta <gustavoqnt40@gmail.com>
2020-07-22 13:29:14 -0300

    ci: log username

--- tests/mplayce/client.test.ts
+++ tests/mplayce/client.test.ts
@@ -9,6 +9,8 @@ describe('MusicPlayceClient', function () {
 		const username = process.env.CLIENT_TEST_USERNAME as string;
 		const password = process.env.CLIENT_TEST_PASSWORD as string;
 
+		console.log('username', username);
+
 		const client = await new MusicPlayceClient({ beta: true }).authenticate({
 			username,
 			password
